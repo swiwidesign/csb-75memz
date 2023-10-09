b497422524c53cf5c87ab2b2cae64a845b7e17b1
swiwidesign <hello@swiwi.me>
2023-10-09 10:38:59 +0200

    Update muva-script.js

--- muva-script.js
+++ muva-script.js
@@ -100,7 +100,7 @@ $(".section.is-intro").each(function (index) {
   });
   tlLogo.from(targetElement, {
     y: isMobile ? "-130%" : "-115%" ,
-    width: isDesktop ? "10cqi": "100cqi",
+    width: isDesktop ? "80cqi": "100cqi",
     duration: 1
   });
 });
