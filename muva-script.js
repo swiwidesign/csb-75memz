608dc2e1ddd90f6963a1b06d672d0f5caa99d2e3
swiwidesign <114654312+swiwidesign@users.noreply.github.com>
2024-01-25 16:17:40 +0100

    sd

--- muva-script.js
+++ muva-script.js
@@ -3,7 +3,7 @@ window.addEventListener("DOMContentLoaded", (event) => {
     "use strict";
     if (Webflow.env("editor") === undefined) {
         const lenis = new Lenis({
-            duration: 1.2
+            duration: 0
             , easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t))
             , lerp: 0.1
             , wheelMultiplier: 0.7
